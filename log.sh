2d155819020032d977bf74b85eaf102f7ad9cb2d
Derek Sorensen <d@dhsorens.com>
2026-02-13 17:31:45 +0000

    remove old publicity for now

---
+++ log.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DEST_DIR="${1:-logs}"
+NUM_FILES="${2:-1}"
+
+for ((i=1; i<=NUM_FILES; i++)); do
+    NEW_FILE=$(forester new forest.toml --dest="trees/$DEST_DIR/" --prefix=dhsorens)
+    if [ -n "$NEW_FILE" ]; then
+        cursor "$NEW_FILE"
+    fi
+done
+
